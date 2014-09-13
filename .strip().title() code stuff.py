e3ac3a4a4cb114fab296c38ee56630684d424f5f
mar10j <youthefluff@gmail.com>
2014-09-12 20:23:40 -0400

    .strip().title() lesson plus printing variables

---
+++ .strip().title() code stuff.py
@@ -0,0 +1,12 @@
+name = input ("What is your name?").strip().title()
+#.strip() makes any uneeded spaces work .title() makes all words start with a capital latter, .lower() makes everything lowercase. Thats all you need to know about that for now.
+print("Hello there " + name)
+
+
+
+#This may seem pointless but it will be used in many different ways.
+#You can print any variable like this,
+print(name)
+#That will print the variable, later you will use this in funtions and importing code.
+
+
