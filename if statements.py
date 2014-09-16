2e227c5b7807d018988d0a3c717b93ab45077903
mar10j <youthefluff@gmail.com>
2014-09-16 19:53:34 -0400

    If statements

---
+++ if statements.py
@@ -0,0 +1,17 @@
+name = input("What is your name?").strip().title()
+
+
+if name == ("Jack"):
+    input("COPYCAT!")
+
+elif name == ("Roxy"):
+    input("OH HAI ROXY!")
+
+
+else:
+    input("Nice to meet you, " + name)
+
+
+
+    
+
